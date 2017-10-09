2f6363bbb8ee8d6ac33b284a9ae4f2eefd9d9322
SHxin <984502528@qq.com>
2017-10-09 21:37:38 +0800

    项目结构搭建

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+    //1. 要知道用户请求网页的时候连接的格式
+    // http://studyit.com/index.php/文件夹名称/文件名称
+
+
+    //2. 需要通过php代码获取到用户要请求的文件夹名称以及文件名
+
+    //如果用户没有传递文件夹以及文件名的信息，我们应该让他默认显示首页
+
+    $path = "/dashboard/index";
+
+    // var_dump($_SERVER);
+    if(array_key_exists('PATH_INFO', $_SERVER)){
+        $path = $_SERVER['PATH_INFO'];
+    }
+
+    //3. 获取到文件夹名称以及文件名之后，要将文件拿到，返回给浏览器
+
+    if(file_exists("views/".$path.".html")){
+        include "views/".$path.".html";
+    }else{
+        header("HTTP/1.1 404 NotFound");
+        echo "404";
+    }
+
+    
+?>
