37f8b55eba8317c6690d69c2f2b816f151ea8c9c
pm <pm@123.com>
2017-10-10 17:30:59 +0800

    完成登录功能以及侧变栏用户信息展示的功能

--- views/static/js/common.js
+++ views/static/js/common.js
@@ -1,8 +1,15 @@
+$(function(){
 
-	NProgress.start();
+	//如果不是在登录页面，才需要从cookie中获取用户数据然后展示在页面
+	if(location.pathname != "/dashboard/login"){
+		//从cookie中获取userinfo的信息
+		var userinfo = $.cookie("userinfo");
+		userinfo = JSON.parse(userinfo);
 
-	NProgress.done();
+		//使用模板引擎将获取到的信息展示到侧边栏
+		var html = template("profile-tpl", userinfo);
+		$("#user-info").html(html);
+	}
 
-	$('.navs ul').prev('a').on('click', function () {
-		$(this).next().slideToggle();
-	});
+	
+})
